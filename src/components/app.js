cf6b6de08ecd61090ebd36bbb2c08444a072d4b3
Sara Vieira <sara.vieira@mindera.com>
2017-07-10 11:59:48 +0100

    fix tabbing

--- src/components/app.js
+++ src/components/app.js
@@ -4,14 +4,12 @@ import { Router } from 'preact-router';
 import Header from './header';
 import Home from '../routes/home';
 import Profile from '../routes/profile';
-// import Home from 'async!./home';
-// import Profile from 'async!./profile';
 
 export default class App extends Component {
   /** Gets fired when the route changes.
-	 *	@param {Object} event		"change" event from [preact-router](http://git.io/preact-router)
-	 *	@param {string} event.url	The newly routed URL
-	 */
+  *	@param {Object} event		"change" event from [preact-router](http://git.io/preact-router)
+  *	@param {string} event.url	The newly routed URL
+  */
   handleRoute = e => {
     this.currentUrl = e.url;
   };
